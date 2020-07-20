aa9669668c21fa9fc853972829a59a42c5af23a4
sunny <sunnydinhtran@gmail.com>
2020-07-20 16:47:40 -0500

    bug seemingly fixed, not how i like it however

--- client/src/components/Portrait.js
+++ client/src/components/Portrait.js
@@ -39,7 +39,7 @@ class Customers extends Component {
     return array;
   }
 
-  getNewPortrait() {
+  async getNewPortrait() {
     var portrait = this.state.portraits[Math.floor(Math.random() * this.state.portraits.length)]
     while (this.state.svg && this.state.name === portrait.name)
       portrait = this.state.portraits[Math.floor(Math.random() * this.state.portraits.length)]
@@ -49,6 +49,8 @@ class Customers extends Component {
     for (var i = 0; i < shuffledColors.length; i++)
       colorsMap[shuffledColors[i]] = (i+1)
 
+    console.log(portrait.colors)
+
     // and black and white, which are very important for shading and mixing colors
     if (!portrait.colors.includes('black'))
       portrait.colors.push('black')
@@ -57,20 +59,21 @@ class Customers extends Component {
 
     // 20% chance of breaking down a color to is components for a challenge :)
     // what if theres light brown but theres no brown in the list?
+    var newColors = []
     for (var i = 0; i < portrait.colors.length; i++) {
       const currentColor = portrait.colors[i]
       const itsComponentColors = this.props.colors[currentColor].mixOf
-      if (itsComponentColors && Math.floor(Math.random() * 5 + 1) == 5) {
+      if (itsComponentColors && Math.floor(Math.random() * 5 + 1) === 5) {
         for (var j = 0; j < itsComponentColors.length; j++) {
           if (!portrait.colors.includes(itsComponentColors[j]))
-            portrait.colors.push(itsComponentColors[j])
+            newColors.push(itsComponentColors[j])
         }
-        // portrait.colors.splice(i, i + 1);
-        // --i;
         console.log(currentColor + " HAS BEEN BROKEN DOWN TO " + this.props.colors[currentColor].mixOf )
-      }
+      } else
+        newColors.push(currentColor)
     }
-    portrait.colors = this.shuffle(portrait.colors)
+    newColors = this.shuffle(newColors)
+    portrait.colors = newColors
 
     console.log(portrait.colors)
 
@@ -78,7 +81,6 @@ class Customers extends Component {
       name: portrait.name,
       artist: portrait.artist,
       svg: portrait.file,
-      colors: portrait.colors,
       key: colorsMap,
       palette: portrait.colors
     })
