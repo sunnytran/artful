bb300727090c2113b36442e38d42c466dea6817f
sunny <sunnydinhtran@gmail.com>
2020-07-22 14:22:31 -0500

    using react-svgmt

--- client/src/components/Portrait.js
+++ client/src/components/Portrait.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import { SvgLoader, SvgProxy } from 'react-svgmt';
 
 class Portrait extends Component {
   constructor(props) {
@@ -7,17 +8,13 @@ class Portrait extends Component {
   }
 
   render() {
-    const svgComponent = () => {
-<svg width="200" height="200"> 
-  <circle cx="80" cy="80" r="50"
-  stroke="black" stroke-width="2" fill="grey" /> 
-</svg> 
-    }
-
     return (
       <div>
         <h1>{this.props.portrait.name} by {this.props.portrait.artist}</h1>
-        <svgComponent></svgComponent>
+        
+        <SvgLoader svgXML={this.props.portrait.file}>
+
+        </SvgLoader>
         {/* <div dangerouslySetInnerHTML={{ __html: this.props.portrait.file }}></div> */}
       </div>
     );
